47e04ebc6ec6fda881f5dae2780835d7a4bd54ad
unknown <imed.siala@gmail.com>
2024-04-01 13:38:20 +0100

    first commitcccddss

--- src/state/api.js
+++ src/state/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export const getArticles= async () => {
-    return await axios.get("https://www.24-crm.online/webservice/api/ARTICLEs?coddos=DG211&serveur=/]QPMFD&user=tb&pwd=Qpmztpgubinfenf");
+    return await axios.get("http://www.24-crm.online/webservice/api/ARTICLEs?coddos=DG211&serveur=/]QPMFD&user=tb&pwd=Qpmztpgubinfenf");
   }

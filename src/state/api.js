238610c18d0823ec234343dd34603f01ad1ec0ba
unknown <imed.siala@gmail.com>
2024-04-03 13:56:06 +0100

    first commithhdddddd

--- src/state/api.js
+++ src/state/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export const getArticles= async () => {
-    return await axios.get("http://www.24-crm.online/webservice/api/ARTICLEs?coddos=gd211&serveur=/]QPMFD&user=tb&pwd=Qpmztpgubinfenf%7Bhboj3135&database=PSGD21NEW");
+    return await axios.get("https://www.24-crm.online/webservice/api/ARTICLEs?coddos=gd211&serveur=/]QPMFD&user=tb&pwd=Qpmztpgubinfenf%7Bhboj3135&database=PSGD21NEW");
   }
